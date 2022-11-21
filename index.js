0b231b4cd4e0c01ca6c4c8a0b0cd5fc20d5d1c8b
RGeorge482 <ratagheorghita@yahoo.com>
2022-11-21 12:23:05 +0000

    ssppractical

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const http = require('http'), 
-      express = require('express'),
-      fs = require("fs"),
-      xmlParse = require('xslt-processor').xmlParse,
-      xsltProcess = require('xslt-processor').xsltProcess,
-      router = express(),
-      server = http.createServer(router);
+const   http = require('http'), //HTTP server
+        express = require('express'), //Handling HTTP requests & routing
+        fs = require('fs'), //File system functionalities
+        xmlParse = require('xslt-processor').xmlParse, //XML handling
+        xsltProcess = require('xslt-processor').xsltProcess, //XSLT handling
+        router = express(), //Init our router
+        server = http.createServer(router); //Init our server
 
+router.get('/', function(req, res) {
 
-router.get('/', function(req, res){
     res.writeHead(200, {'Content-Type' : 'text/html'});
 
-    let xml = fs.readFileSync('menu.xml', 'uft8'),
-        xsl = fs.readFileSync('menu.xsl', 'uft8');
+    let xml = fs.readFileSync('menu.xml', 'utf8'),
+        xsl = fs.readFileSync('menu.xsl', 'utf8');
 
-        xml = xmlParse(xml);
-        xsl = xmlParse(xsl);
+    xml = xmlParse(xml);
+    xsl = xmlParse(xsl);
 
-        let html = xsltProcess(xml, xsl);
+    let html = xsltProcess(xml, xsl);
 
-        res.end(html.toString());
-})      
+    res.end(html.toString());
+});
 
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
     const addr = server.address();
-    console.log("Server listening at ", addr.address + ":" + addr.port);
-})
+    console.log("Server listening at", addr.address + ":" + addr.port)
+});
